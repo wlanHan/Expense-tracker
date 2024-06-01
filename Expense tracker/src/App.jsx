fd87131a42ca3a8986868fd0cf1223fea1bddad2
Efe Han Oz <hanstudiocontact@gmail.com>
2024-06-01 15:30:54 +0300

    first commit

---
+++ Expense tracker/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+import Header from './Components/Header'
+import Balance from './Components/Balance'
+import IncomeExpenses from './Components/IncomeExpenses'
+
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+      </div>
+    </div>
+  )
+}
+
+export default App
