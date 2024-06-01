ff03e5223bc7ca6f79617778145dad21d35038cc
Efe Han Oz <hanstudiocontact@gmail.com>
2024-06-01 16:14:00 +0300

    Income & Expense +

--- Expense tracker/src/Components/Transaction.jsx
+++ Expense tracker/src/Components/Transaction.jsx
@@ -4,7 +4,7 @@ const Transaction = ({ transaction }) => {
     const sign = transaction.amount < 0 ? '-' : '+';
     return (
         <>
-            <li className="minus">
+            <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
                 {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
             </li>
         </>
