483c66123671cc34c67bead5bba36d5252ddad39
Efe Han Oz <hanstudiocontact@gmail.com>
2024-06-01 15:41:51 +0300

    Transaction +

--- Expense tracker/src/App.jsx
+++ Expense tracker/src/App.jsx
@@ -3,7 +3,8 @@ import './App.css'
 import Header from './Components/Header'
 import Balance from './Components/Balance'
 import IncomeExpenses from './Components/IncomeExpenses'
-
+import TransactionList from './Components/TransactionList'
+import AddTransaction from './Components/AddTransaction'
 
 const App = () => {
   return (
@@ -12,6 +13,8 @@ const App = () => {
       <div className="container">
         <Balance />
         <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
       </div>
     </div>
   )
